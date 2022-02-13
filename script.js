58bb2eeb278383d9b30025fcdd3857208e45b664
YossraElHaddad <nspecial56@gmail.com>
2022-02-13 21:26:07 +0200

    Add complete javascript file

---
+++ script.js
@@ -0,0 +1,95 @@
+let result = document.querySelector('.result');
+let input = "";
+let textInput = [];
+let operatorInput = [];
+const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('.numbers .number');
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    if (num2 == 0)
+        return "Error";
+    else
+        return (num1 / num2);
+}
+
+function operate(operator, num1, num2) {
+    let result = 0;
+    num1 = +num1;
+    num2 = +num2;
+    if (operator === "+")
+        result = add(num1, num2);
+    else if (operator === "-")
+        result = subtract(num1, num2);
+    else if (operator === "*")
+        result = multiply(num1, num2);
+    else
+        result = divide(num1, num2);
+    return result;
+
+}
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+
+        if (button.className === "clear") {
+            input = "";
+            result.textContent = "";
+            textInput = [];
+            operatorInput = [];
+
+        } else if (button.className === "positive-negative") {
+            if (result.textContent) {
+                result.textContent = "" + (- +(result.textContent));
+                input = result.textContent;
+            }
+
+        } else if (button.className === "percentage") {
+            if (result.textContent) {
+                result.textContent = "" + (+(result.textContent) * 0.01);
+                input = result.textContent;
+            }
+        } else if (button.className === "number") {
+            input += button.textContent;
+            result.textContent = input;
+
+        } else if (button.className === "operator") {
+
+            if (operatorInput.length < 2 && textInput.length < 2) {
+                let operator = button.textContent;
+                textInput.push(result.textContent);
+                operatorInput.push(operator);
+                input = "";
+
+                if (textInput.length == 2 && operatorInput.length == 2) {
+                    input = operate(operatorInput[0], textInput[0], textInput[1]).toFixed(3).toString();
+                    textInput = [];
+                    if (operatorInput[1] === "=")
+                        operatorInput = [];
+                    else {
+                        operatorInput.shift();
+                        textInput.push(input);
+                    }
+
+                    //textInput = textInput.slice(0,2);
+                    result.textContent = input;
+                    input = "";
+                }
+            }
+
+        }
+
+    });
+});
